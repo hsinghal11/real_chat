ade1d840635398e4a5f49af82838e232a9f7a6b5
Himanshu Singhal <himanshusinghal11082004@gmail.com>
2025-06-17 23:32:16 +0530

    Implemented Socket

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,58 @@
+import { useEffect, useMemo, useState } from "react";
 import { io } from "socket.io-client"
 
 
 function App() {
-  const socket = io("http://localhost:3000", {
-    autoConnect: false,
-  });
-  socket.connect();
-  socket.emit("hello", "Hello from client!");
-  socket.on("hello", (data) => {
-    console.log(data);
-  });
+  const socket = useMemo(
+    () =>
+      io("http://localhost:8000", {
+        transports: ["websocket"],
+        withCredentials: true,
+      }),
+    []
+  );
+
+  const [messages, setMessages] = useState<string[]>([]);
+  const [message, setMessage] = useState<string>("");
+  
+  useEffect(() => {
+    socket.on("connect", () => {
+      console.log(`connected to server with id : ${socket.id}`);
+    });
+
+    socket.on("hello", (data) => {
+      console.log("Received from server:", data);
+      setMessages((prev) => [...prev, data]);
+    });
+
+    return () => {
+      socket.disconnect();
+      console.log("Socket disconnected");
+    };
+  }, []);
+
+  const sendInput = ()=>{
+    socket.emit("hello", "Hello from client with ID: "+ socket.id+ "with message: "+ message);
+    setMessage("");
+  }
+
   return (
     <div>
-
+      <h1>Socket prep started</h1>
+      <input
+        type="text"
+        placeholder="typeHere"
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+      />
+      <button onClick={sendInput}>send message</button>
+      <ul>
+        {messages.map((msg, index) => (
+          <li key={index}>{msg}</li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
 
 export default App
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,8 @@
 import express from "express";
-import { Server } from "socket.io";
+import { Server, Socket } from "socket.io";
 import http from "http";
-import { ILogObj, Logger } from "tslog";
 import cookieParser from "cookie-parser";
-import { appendFileSync } from "fs";
+import cors from "cors";
 
 
 const app = express();
@@ -19,18 +18,17 @@ const io = new Server(server, {
     }
 });
 
-const log: Logger<ILogObj> = new Logger();
-log.attachTransport((logObj) => {
-  appendFileSync("logs.txt", JSON.stringify(logObj) + "\n");
-});
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true,
+})) 
 
-
-io.on("connection", (socket) => {
+io.on("connection", (socket: Socket) => {
     console.log("USer connected", socket.id);  
     socket.on("hello", (data) => {
         // log.info("Hello event received", data);
         console.log("Hello event received", data);
-        socket.emit("hello", "Hello from server!");
+        io.emit("hello", `${data}`);
     });
 });
 
@@ -49,6 +47,5 @@ app.use("/api/v1/user", userRouter);
 // });
 
 server.listen(PORT, () => {
-  log.info(`Server is running on http://localhost:${PORT} `);
   console.log(`Server is running on http://localhost:${PORT}`);
 });
